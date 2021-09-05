895d34bcd4bf5c36cc9cf2486a8b470662fabda6
juicyjung <travelandi01@gmail.com>
2021-09-05 11:48:53 +0900

    homework 1 fin

--- Algoritihm/Algoritihm/homework_1.cpp
+++ Algoritihm/Algoritihm/homework_1.cpp
@@ -3,66 +3,88 @@
 #include <string.h>
 
 int pancakes[30];
-int stack[30];
+int output[30];
 int i = 0;
+int r = 0;
 
 void flip(int f) {
-	for (int n = 0; n < i; n++)
-		stack[n] = pancakes[i - n - f];
+	int newpancakes[30] = { 0 };
+
+	for (int n = 0; n < i - f + 1; n++) {
+		newpancakes[n] = pancakes[i - n - f];
+	}
+
+	for (int n = 0; n < i - f + 1; n++) {
+		pancakes[n] = newpancakes[n];
+	}
+
+	output[r] = f;
+	r++;
+
+	/* printf("flip! %d\n", f);
+	for (int j = 0; j < i; j++)
+		printf("%d ", pancakes[j]); */
 }
 
+void stacking(int i) {
+	for (int k = i; k > 0; k--) {
+		int max = 0, max_num = 0;
+
+		if (k == 1)
+			output[r] = 0;
+		else {
+			for (int j = 0; j < k; j++) {
+				if (pancakes[j] > max) {
+					max = pancakes[j];
+					max_num = j;
+				}
+			}
+
+			if (max_num == 0) {
+				flip(i - k + 1);
+			}
+			else if (max_num != k - 1) {
+				flip(i - max_num);
+				flip(i - k + 1);
+			}
+		}
+	}
+}
 
 int main() {
 	char input[100];
 	char* str = NULL;
 
-	gets_s(input, sizeof(input));
-
-	char* ptr = strtok_s(input, " ", &str);
-	pancakes[i] = atoi(ptr);
-
 	while (1) {
-		i++;
-		printf("%s\n", ptr);
-		ptr = strtok_s(NULL, " ", &str);
-		if (ptr == NULL)
-			break;
+		gets_s(input, sizeof(input));
+
+		char* ptr = strtok_s(input, " ", &str);
 		pancakes[i] = atoi(ptr);
-	}
 
-	int max = 0;
-	int max_num;
-
-	for (int j = 0; j < i; j++) {
-		printf("%d ", pancakes[j]);
-
-		if (pancakes[j] > max) {
-			max = pancakes[j];
-			max_num = j;
+		while (1) {
+			i++;
+			ptr = strtok_s(NULL, " ", &str);
+			if (ptr == NULL)
+				break;
+			pancakes[i] = atoi(ptr);
 		}
+
+		stacking(i);
+
+		for (int j = 0; j < i; j++)
+			printf("%d ", pancakes[j]);
+
+		printf("\n");
+
+		for (int j = 0; j <= r; j++)
+			printf("%d ", output[j]);
+
+		printf("\n\n");
+
+		i = 0;
+		r = 0;
+		str = NULL;
 	}
-
-	fix = 1;
-
-	if (max_num == i - 1) {
-		fix++;
-		i = i - fix;
-	}
-	else if (max_num == 0) {
-		flip(fix);
-		fix++;
-		i = i - fix;
-	}
-	else {
-		flip(i - max_num);
-		flip(fix);
-		fix++;
-		i = i - fix;
-	}
-
-
-
-
 
 	return 0;
 }
