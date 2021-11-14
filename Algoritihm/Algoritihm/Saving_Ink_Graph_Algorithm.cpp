2e525623fa52471e1f6b642b45c6f1677e0c868d
juicyjung <travelandi01@gmail.com>
2021-11-14 23:23:20 +0900

    term project saving ink

--- Algoritihm/Algoritihm/Saving_Ink_Graph_Algorithm.cpp
+++ Algoritihm/Algoritihm/Saving_Ink_Graph_Algorithm.cpp
@@ -27,52 +27,51 @@ void get_distance(DOT dot[], int num, double distance_table[30][30], int used_se
 	}
 }
 
-int main() {
-	int num;
-	double distance_table[30][30];
-	int used_set[30];
+void get_minimum_ink(int num, double distance_table[30][30], int used_set[30], double * output) {
 	int selected_set[30];
 	int selceted_num = 0;
 	double min = 9999;
 	int min_i;
 	int min_j;
-	double output = 0;
-
-	get_input(&num);
-
-	get_distance(dot, num, distance_table, used_set);
 
 	selected_set[selceted_num] = 0;
 	selceted_num++;
 
-	for (int i = 0; i < num; i++) {
-		for (int j = 0; j < i; j++) {
-			printf("%lf ", distance_table[i][j]);
-		}
-		printf("\n");
-	}
-
 	while (num != selceted_num) {
 		for (int i = 0; i < num; i++) {
-			for (int j = 0; j < i; j++) {
+			for (int j = i + 1; j < num; j++) {
 				for (int s = 0; s < selceted_num; s++) {
-					if (i == selected_set[s] && used_set[i] == 0) {
+					if (i == selected_set[s] && (used_set[i] == 0 || used_set[j] == 0)) {
 						if (min > distance_table[i][j]) {
 							min = distance_table[i][j];
 							min_i = i;
 							min_j = j;
-							printf("Good\n");
 						}
 					}
 				}
 			}
 		}
 		used_set[min_i] = 1;
+		used_set[min_j] = 1;
+
 		selected_set[selceted_num] = min_j;
 		selceted_num++;
-		output += min;
+		*output += min;
 		min = 9999;
 	}
+}
+
+int main() {
+	int num;
+	double distance_table[30][30];
+	int used_set[30];
+	double output=0;
+
+	get_input(&num);
+
+	get_distance(dot, num, distance_table, used_set);
+
+	get_minimum_ink(num, distance_table, used_set, &output);
 
 	printf("Output : %lf\n", output);
 
